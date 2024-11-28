68d57f40643bdec4b119826a8e7840b1aa1b11c3
Nurislom <anurislom425@gmail.com>
2024-11-28 22:30:58 +0500

    html qoshdim

--- homework 6-oy 3-dars/new/views.py
+++ homework 6-oy 3-dars/new/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.core.handlers.wsgi import WSGIRequest
 # Create your views here.
+def mainpage(request: WSGIRequest):
+    return render(request,'index.html')
